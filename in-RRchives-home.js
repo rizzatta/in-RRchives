235177ce7ae2b56bab377f29b05077b5db3ef343
rizzatta <rizzacolis18@gmail.com>
2026-03-23 23:45:44 +0800

    Refined navigation bar disappearance-reappearance

--- in-RRchives-home.js
+++ in-RRchives-home.js
@@ -1,15 +1,11 @@
-<script>
-    let lastScrollY = window.scrollY;
-    const navBar = document.querySelector('.main-navigation-bar');
+let lastScrollY = window.scrollY;
+const navBar = document.querySelector('.main-navigation-bar');
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > lastScrollY) {
-            // Scrolling Down - Hide Nav
-            navBar.classList.add('nav-hidden');
-        } else {
-            // Scrolling Up - Show Nav
-            navBar.classList.remove('nav-hidden');
-        }
-        lastScrollY = window.scrollY;
-    });
-</script>
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 10 && window.scrollY > lastScrollY) {
+        navBar.classList.add('nav-hidden');
+    } else {
+        navBar.classList.remove('nav-hidden');
+    }
+    lastScrollY = window.scrollY;
+});
